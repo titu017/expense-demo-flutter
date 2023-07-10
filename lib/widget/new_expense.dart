7e81d32558f58167fb8a404698f7a1d89c722f3c
moinulislam <titu017@gmail.com>
2023-07-10 09:59:49 +0100

    overlay modal full screen/scrollable and exit when saved new expense

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -84,14 +84,13 @@ class _NewExpenseState extends State<NewExpense> {
           date: _selectedDate!,
           category: _selectedCategory),
     );
+    Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(
-        16,
-      ),
+      padding: const EdgeInsets.fromLTRB(16, 64, 16, 16),
       child: ListView(
         children: [
           //Title starts from here
